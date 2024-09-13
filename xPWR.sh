8fc3da26c6e5b859c527ada9669fc46f9b12c4c6
huangyp <harry@geekworm.com>
2024-09-13 20:20:52 +0800

    Change Chinese descriptions to English

--- xPWR.sh
+++ xPWR.sh
@@ -2,19 +2,19 @@
 
 #  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
 
-# 确保有足够的参数被传入  
+# Make sure enough parameters are passed in  
 if [ "$#" -ne 3 ]; then  
   echo "Usage: $0 <pwm_chip> <shutdown_pin> <boot_pin>"  
   exit 1  
 fi  
 
-PWMCHIP=$1    
+GPIOCHIP=$1    
 SHUTDOWN=$2  
 BOOT=$3
 
-# 检查传入的参数是否为整数
+# Checks if the passed parameter is an integer
 re='^[0-9\.]+$'
-if ! [[ $PWMCHIP =~ $re ]] ; then
+if ! [[ $GPIOCHIP =~ $re ]] ; then
    echo "error: pwm_chip is not a number" >&2; exit 1
 fi
 
@@ -29,11 +29,11 @@ fi
 REBOOTPULSEMINIMUM=200  
 REBOOTPULSEMAXIMUM=600  
   
-# 初始化BOOT引脚为1  
-gpioset $PWMCHIP $BOOT=1  
+# Initialize the BOOT pin to 1
+gpioset $GPIOCHIP $BOOT=1  
   
 while [ 1 ]; do  
-  shutdownSignal=$(gpioget $PWMCHIP $SHUTDOWN)  
+  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
   if [ $shutdownSignal -eq 0 ]; then  
     sleep 0.2  
   else  
@@ -45,7 +45,7 @@ while [ 1 ]; do
         sudo poweroff  
         exit  
       fi  
-      shutdownSignal=$(gpioget $PWMCHIP $SHUTDOWN)  
+      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
     done  
     if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then  
       echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."  
--- xSoft.sh
+++ xSoft.sh
@@ -4,20 +4,20 @@
 
 # 检查是否提供了足够的命令行参数
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <pwm_chip> <button_pin>" >&2
+    echo "Usage: $0 <gpio_chip> <button_pin>" >&2
     exit 1
 fi
 
 PWMCHIP=$1
 BUTTON=$2
 
-SLEEP=4
+SLEEP=2
 
 # 检查传入的参数是否为整数
 
 re='^[0-9\.]+$'
 if ! [[ $PWMCHIP =~ $re ]] ; then
-   echo "error: pwm_chip is not a number" >&2; exit 1
+   echo "error: gpio_chip is not a number" >&2; exit 1
 fi
 
 if ! [[ $BUTTON =~ $re ]] ; then
@@ -26,4 +26,10 @@ fi
 
 echo "Your device will be shutting down in $SLEEP seconds..."
 
-gpioset --mode=time -s $SLEEP $PWMCHIP $BUTTON=1
+gpioset $GPIOCHIP $BUTTON=1
+
+sleep $SLEEP
+
+# Restore GPIO
+# This step is necessary, otherwise you will have to press the onboard button twice to turn on the device, and the same applies to the AUTO ON function.
+gpioset $GPIOCHIP $BUTTON=0
