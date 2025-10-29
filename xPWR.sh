1c6d0b839d010c8339c176ec0b4573b985d277ef
TB-X1C\huangyp <harry@geekworm.com>
2025-10-29 23:13:18 +0800

    Update for Trixie OS

--- xPWR.sh
+++ xPWR.sh
@@ -1,56 +1,98 @@
-#!/bin/bash  
+#!/bin/bash
+
+# In October 2025, the Raspberry Pi OS was updated from Bookworm to Trixie. The libgpiod library in Trixie has been upgraded to version 2.2.1. 
+# The syntax of the gpioset command has changed, so harry@geekworm.com updated this script.
+# Refer to https://libgpiod.readthedocs.io/en/latest/gpio_tools.html#examples
+
+# Global debugging switch (0=off, 1=on)
+DEBUG=0
 
 #  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
+# Log debug info, only when DEBUG=1 above
+function logdebug {
+    if [ "$DEBUG" -eq 1 ]; then
+        echo "$@" >&2
+    fi
+}
 
-# Make sure enough parameters are passed in  
-if [ "$#" -ne 3 ]; then  
-  echo "Usage: $0 <pwm_chip> <shutdown_pin> <boot_pin>"  
-  exit 1  
-fi  
+# Log errors
+function logerr {
+    echo "$@" >&2
+}
 
-GPIOCHIP=$1    
-SHUTDOWN=$2  
-BOOT=$3
+main() {
+  local GPIOCHIP=$1
+  local SHUTDOWN=$2
+  local BOOT=$3
 
-# Checks if the passed parameter is an integer
-re='^[0-9\.]+$'
-if ! [[ $GPIOCHIP =~ $re ]] ; then
-   echo "error: gpio_chip is not a number" >&2; exit 1
+  # Make sure enough parameters are passed in
+  if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <pwm_chip> <shutdown_pin> <boot_pin>"
+    exit 1
+  fi
+
+  # Checks if the passed parameter is an integer
+  re='^[0-9\.]+$'
+  if ! [[ $GPIOCHIP =~ $re ]] ; then
+    logerr "error: pwm_chip is not a number"
+    exit 1
+  fi
+
+  if ! [[ $SHUTDOWN =~ $re ]] ; then
+    logerr "error: shutdown_pin is not a number"
+    exit 1
+  fi
+
+  if ! [[ $BOOT =~ $re ]] ; then
+    logerr "error: button_pin is not a number"
+    exit 1
+  fi
+
+  local REBOOTPULSEMINIMUM=200
+  local REBOOTPULSEMAXIMUM=600
+  local pulseStart
+  local shutdownSignal
+  local output
+
+  # Initialize the BOOT pin to 1
+  # 必须加上-t0参数，否则堵塞在这里了。
+  gpioset -c $GPIOCHIP -t0 $BOOT=1
+
+  while [ 1 ]; do
+    output=$(gpioget -c $GPIOCHIP $SHUTDOWN)
+    # "5"=inactive, get the string after =
+    shutdownSignal=$(echo "$output" | awk -F '=' '{print $NF}' | tr -d '"')
+    #logdebug $output
+    logdebug $shutdownSignal
+    if [ "$shutdownSignal" = "inactive" ]; then
+      sleep 0.2
+    else
+      pulseStart=$(date +%s%N | cut -b1-13)
+
+      while [ "$shutdownSignal" = "active" ]; do
+        sleep 0.02
+        if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
+          echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."
+          sudo poweroff
+          exit
+        fi
+        # shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
+        output=$(gpioget -c $GPIOCHIP $SHUTDOWN)
+        shutdownSignal=$(echo "$output" | awk -F '=' '{print $NF}' | tr -d '"')
+        #logdebug $output
+        logdebug $shutdownSignal
+      done
+      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
+        echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."
+        sudo reboot
+        exit
+      fi
+    fi
+  done
+
+}
+
+# Script entry point
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
 fi
-
-if ! [[ $SHUTDOWN =~ $re ]] ; then
-   echo "error: shutdown_pin is not a number" >&2; exit 1
-fi
-
-if ! [[ $BOOT =~ $re ]] ; then
-   echo "error: button_pin is not a number" >&2; exit 1
-fi
-
-REBOOTPULSEMINIMUM=200  
-REBOOTPULSEMAXIMUM=600  
-  
-# Initialize the BOOT pin to 1
-gpioset $GPIOCHIP $BOOT=1  
-  
-while [ 1 ]; do  
-  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
-  if [ $shutdownSignal -eq 0 ]; then  
-    sleep 0.2  
-  else  
-    pulseStart=$(date +%s%N | cut -b1-13)  
-    while [ $shutdownSignal -eq 1 ]; do  
-      sleep 0.02  
-      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then  
-        echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."  
-        sudo poweroff  
-        exit  
-      fi  
-      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
-    done  
-    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then  
-      echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."  
-      sudo reboot  
-      exit  
-    fi  
-  fi  
-done
--- xSoft.sh
+++ xSoft.sh
@@ -2,22 +2,26 @@
 
 #  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
 
-# 检查是否提供了足够的命令行参数
+# In October 2025, the Raspberry Pi OS was updated from Bookworm to Trixie. The libgpiod library in Trixie has been upgraded to version 2.2.1. 
+# The syntax of the gpioset command has changed, so harry@geekworm.com updated this script.
+# Refer to https://libgpiod.readthedocs.io/en/latest/gpio_tools.html#examples
+
+# Check if enough command line arguments were provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <pwm_chip> <button_pin>" >&2
+    echo "Usage: $0 <gpio_chip> <button_pin>" >&2
     exit 1
 fi
 
-PWMCHIP=$1
+GPIOCHIP=$1
 BUTTON=$2
 
-SLEEP=2
+SLEEP=4
 
-# 检查传入的参数是否为整数
+# Checks if the passed parameter is an integer
 
 re='^[0-9\.]+$'
-if ! [[ $PWMCHIP =~ $re ]] ; then
-   echo "error: pwm_chip is not a number" >&2; exit 1
+if ! [[ $GPIOCHIP =~ $re ]] ; then
+   echo "error: gpio_chip is not a number" >&2; exit 1
 fi
 
 if ! [[ $BUTTON =~ $re ]] ; then
@@ -26,10 +30,12 @@ fi
 
 echo "Your device will be shutting down in $SLEEP seconds..."
 
-gpioset $PWMCHIP $BUTTON=1
+# gpioset -c $GPIOCHIP -p 3s -t0 $BUTTON=1
+gpioset -c $GPIOCHIP -t0 $BUTTON=1
 
 sleep $SLEEP
 
 # Restore GPIO
 # This step is necessary, otherwise you will have to press the onboard button twice to turn on the device, and the same applies to the AUTO ON function.
-gpioset $PWMCHIP $BUTTON=0
+gpioset -c $GPIOCHIP -t0 $BUTTON=0
+#gpioset -c $GPIOCHIP -p 20ms -t0 $BUTTON=0
